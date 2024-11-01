f3896058207a172068cac8b88d76023474432186
leesahanders <lisa.anders@posit.co>
2024-11-01 15:53:02 +0000

    project initialization, gitignore updates

---
+++ R/env_management/env_management_dynamic_update.R
@@ -0,0 +1,12 @@
+library(renv)
+
+## Make sure renv is updated 
+
+## Update one project from terminal 
+#R -e 'renv::install(); renv::snapshot()'
+#From Kevin: Calling renv::install() without arguments will install the latest-available version of any project dependencies.
+
+
+
+
+

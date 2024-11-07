c39a3cec3129a156e8746fbc9fbfb3d4487f2f56
leesahanders <lisa.anders@posit.co>
2024-11-07 20:16:51 +0000

    Adding files

--- R/env_management/env_management_dynamic_update.R
+++ R/env_management/env_management_dynamic_update.R
@@ -1,5 +1,7 @@
 library(renv)
 library(tidyverse)
+library(rsconnect)
+library(rstudioapi)
 
 ## List all folders in directory 
 folders <- list.dirs(path = ".", full.names = TRUE, recursive = TRUE) 
@@ -12,17 +14,52 @@ files_renv <- grepl("*renv.lock", c(files))
 
 ## List all folders with a manifest.json file
 files_manifest <- grepl("*manifest.json", c(files))
+
+## List all folders with a .Rproj project file
+files_rproj <- grepl("*.Rproj", c(files))
   
 ## Turn it into a table
-files_tbl <- data.frame(files=c(files), renv=c(files_renv), manifest = c(files_manifest))
+files_tbl <- data.frame(files=c(files), renv=c(files_renv), manifest = c(files_manifest), rproj = c(files_rproj)) %>%
+  mutate(folder = sub("\\/[^/]*$", "", files)) %>% 
+  select(folder, files, renv, manifest, rproj) %>% 
+  mutate(renv2=ifelse(renv, files, "")) %>%
+  mutate(manifest2=ifelse(manifest, files, "")) %>%
+  mutate(rproj2=ifelse(rproj, files, "")) 
+  #mutate(folder = sub("\\.[^.]*$", "", files)) # This removes everything after the last .
+
+proj_tbl <- files_tbl %>% 
+  select(-files) %>% 
+  group_by(folder) %>%
+  summarize(renv1 = max(renv), manifest1 = max(manifest), rproj1 = max(rproj),
+            renv2 = max(renv2), manifest2 = max(manifest2), rproj2 = max(rproj2)) %>%
+  ungroup() %>% 
+  mutate(good = renv1 + manifest1 +rproj1) %>%
+  filter(good == 3)
+
+# https://stackoverflow.com/questions/52471073/remove-or-replace-everything-after-a-specified-character-in-r-strings
+
+i=1
+folder=proj_tbl$folder[i]
+file_renv=proj_tbl$renv2[i]
+file_manifest=proj_tbl$manifest2[i]
+file_rproj=proj_tbl$rproj2[i]
+path = paste0(getwd(),"/",folder)
+
+print(paste0("Starting on ", folder))
+
+openProject(path)
+
   
-
-## Make sure renv is updated 
-
 ## Update one project from terminal 
 #R -e 'renv::install(); renv::snapshot()'
 #From Kevin: Calling renv::install() without arguments will install the latest-available version of any project dependencies.
 
+#- Use `renv::record("renv@1.0.7")` to record renv 1.0.7 in the lockfile.
+#- Use `renv::restore(packages = "renv")` to install renv 1.0.11 into the project library.
 
+#R -e 'renv::record("renv@1.0.11"); renv::restore(packages = "renv"); renv::install(); renv::snapshot(); rsconnect::writeManifest()'
 
+# https://stackoverflow.com/questions/37724057/open-r-project-in-rstudio-programmatically
+#openProject() in package rstudioapi
+# https://pkgs.rstudio.com/rstudioapi/articles/projects.html
 
